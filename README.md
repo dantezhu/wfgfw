7e53b11c2f40b87576b1910093f00e5a2324ea38
dantezhu <dantezhu@qq.com>
2013-12-24 12:50:53 +0800

    fork说明

--- README.md
+++ README.md
@@ -2,4 +2,7 @@ wfgfw
 =====
 
 关键词过滤，不能太直白。
+
+fork from https://github.com/observerss/textfilter
+
 下载词库: https://github.com/observerss/textfilter
--- wfgfw.py
+++ wfgfw.py
@@ -20,7 +20,7 @@
 #=============================================================================
 """
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 from collections import defaultdict
 import re
