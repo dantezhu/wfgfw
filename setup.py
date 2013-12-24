d4a5ea5d3499d945809ef33a5c5fd845fce7ccf2
dantezhu <dantezhu@qq.com>
2013-12-24 12:46:04 +0800

    初始化

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+import wfgfw
+
+setup(
+    name="wfgfw",
+    version=wfgfw.__version__,
+    zip_safe=False,
+    platforms='any',
+    py_modules=['wfgfw'],
+    url="https://github.com/dantezhu/wfgfw",
+    license="BSD",
+    author="dantezhu",
+    author_email="zny2008@gmail.com",
+    description="word filter for gfw",
+    )
